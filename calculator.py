c84f67879a90e3538cc79c960651ac2814efe59b
soundara rajan <soundaraj.thevan@gmail.com>
2018-10-18 18:25:09 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,15 +2,19 @@
 
 def calculator(num1,num2):
     operation = input("enter the which operation you want \n 1.addition \n 2.subtraction \n 3.multiplication \n 4.division \n ")
+    '''This condition done the addition'''
     if operation == 1:
         a = num1 + num2
         print(a)
+    '''This condition done the subtraction'''
     elif operation == 2:
         b = num1 - num2
         print(b)
+    '''This condition done the multiplication'''
     elif operation == 3:
         c = num1 * num2
         print(c)
+    '''This condition done the division'''
     elif operation == 4:
         d = num1 / num2
         print(d)
